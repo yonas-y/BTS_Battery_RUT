30263c990f591a058d6fd5f2b5bc624d89106cc1
Yonas Y <yoni99y@gmail.com>
2025-07-30 17:00:10 +0300

    Added mongoDB connection procedure and paths about the data.

---
+++ app/config.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+from pymongo import MongoClient
+
+# MongoDB connection!
+MONGO_URI = "mongodb://192.168.80.1:27017"
+client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=3000)
+
+# Paths!
+operational_data_dir = Path("../../../Datasets/BTS/csv_files/operational_data/")
+fault_alarm_data_dir = Path("../../../Datasets/BTS/csv_files/fault/")
