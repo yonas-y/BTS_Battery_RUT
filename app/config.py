37b40d5588f8260352589679bbbaf728abec90e2
Yonas Y <yoni99y@gmail.com>
2025-08-01 10:23:16 +0300

    Added a sampling time parameter!

--- app/config.py
+++ app/config.py
@@ -8,3 +8,6 @@ client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=3000)
 # Paths!
 operational_data_dir = Path("../../../Datasets/BTS/csv_files/operational_data/used_btss")
 fault_alarm_data_dir = Path("../../../Datasets/BTS/csv_files/fault/")
+
+# Important parameters!
+sampling_time = 5 / 60  # Sampling time of the time series data!
