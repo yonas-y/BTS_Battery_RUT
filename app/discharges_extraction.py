785580dc52561f314b8117b4bea32757b63a8998
Yonas Y <yoni99y@gmail.com>
2025-08-04 12:13:21 +0300

    Updated the discharge function to return the start and ending timestamps plus the length of each discharge!

--- app/discharges_extraction.py
+++ app/discharges_extraction.py
@@ -20,19 +20,20 @@ def bts_discharges(bts_dataframe):
         discharges_list : List of lists of discharge + length of each discharge
     """
 
-    b = bts_dataframe['Battery Current(A)'].values
-    a = bts_dataframe['AC/DC System Output Current(A)'].values
-    u = b.copy()
-    u[u > 0] = 1  # Positive if charging or stable with small drawn current.
-    u[u < 0] = -1  # When in case of discharging!
+    i_B = bts_dataframe['Battery Current(A)'].values
+    i_AC = bts_dataframe['AC/DC System Output Current(A)'].values
+    timestamp = bts_dataframe['timestamp'].values
+    i_B_copy = i_B.copy()
+    i_B_copy[i_B_copy > 0] = 1  # Positive if charging or stable with small drawn current.
+    i_B_copy[i_B_copy < 0] = -1  # When in case of discharging!
 
-    m = np.flatnonzero(np.diff(u))  # Selects the entries where sign change is greater than zero!!!
+    m = np.flatnonzero(np.diff(i_B_copy))  # Selects the entries where sign change is greater than zero!!!
 
     discharges_list = list()  # Finds negative values($[  )$) in the df!!
     for i in range(len(m) - 1):
         # Append only discharges which are longer than 30mins!
-        if b[m[i] + 1] < 0 and a[m[i] + 1] <= 5 and m[i + 1] - m[i] > 6:
-            discharges_list.append([m[i] + 1, m[i + 1] + 1, m[i + 1] - m[i]])
+        if i_B[m[i] + 1] < 0 and i_AC[m[i] + 1] <= 5 and m[i + 1] - m[i] > 6:
+            discharges_list.append([timestamp[m[i] + 1], timestamp[m[i + 1] + 1], m[i + 1] - m[i]])
 
     return np.array(discharges_list)
 
@@ -40,4 +41,23 @@ def bts_discharges(bts_dataframe):
 db = client["BTS_Dataset"]
 collection = db["bts_oper_data_soc"]
 unique_bts_ids = collection.distinct("bts_id")
+
+# ===== Select all usable discharges from the different BTSs ======= #
+"""
+The selection criteria are:
+    1. Discharges which are reach below some threshold value (46.2V)!
+    2. In some cases the battery current remains negative after the load is disconnected!
+        In such situations remove those values!!
+    3. Store all the discharges starting and ending index in a list!!!!
+
+"""
+all_BTS_discharges = []
+bts_list_final_Upd = []
+
+# Process each bts_id!
+for bts_id in unique_bts_ids:
+    bts_considered = pd.DataFrame(list(collection.find({"bts_id": bts_id})))
+
+    BTS_Discharges = bts_discharges(bts_considered)
+    BTS_Discharges_Sorted = BTS_Discharges[BTS_Discharges[:, 2].astype(int).argsort()[::-1]]
 
