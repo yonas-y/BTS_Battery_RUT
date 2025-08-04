decf2100a9811b61c170fcf050529640bc725cd8
Yonas Y <yoni99y@gmail.com>
2025-08-04 16:09:20 +0300

    Stored the discharges from the BTSs in an output directory.

--- app/config.py
+++ app/config.py
@@ -8,6 +8,7 @@ client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=3000)
 # Paths!
 operational_data_dir = Path("../../../Datasets/BTS/csv_files/operational_data/used_btss")
 fault_alarm_data_dir = Path("../../../Datasets/BTS/csv_files/fault/")
+output_dir = Path("output/")
 
 # Important parameters!
 sampling_time = 5 / 60  # Sampling time of the time series data!
--- app/discharges_extraction.py
+++ app/discharges_extraction.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import pickle
 import os
-from app.config import client
+from app.config import client, output_dir
 
 def bts_discharges(bts_dataframe):
     """
@@ -22,7 +22,6 @@ def bts_discharges(bts_dataframe):
 
     i_B = bts_dataframe['Battery Current(A)'].values
     i_AC = bts_dataframe['AC/DC System Output Current(A)'].values
-    timestamp = bts_dataframe['timestamp'].values
     i_B_copy = i_B.copy()
     i_B_copy[i_B_copy > 0] = 1  # Positive if charging or stable with small drawn current.
     i_B_copy[i_B_copy < 0] = -1  # When in case of discharging!
@@ -33,7 +32,7 @@ def bts_discharges(bts_dataframe):
     for i in range(len(m) - 1):
         # Append only discharges which are longer than 30mins!
         if i_B[m[i] + 1] < 0 and i_AC[m[i] + 1] <= 5 and m[i + 1] - m[i] > 6:
-            discharges_list.append([timestamp[m[i] + 1], timestamp[m[i + 1] + 1], m[i + 1] - m[i]])
+            discharges_list.append([bts_dataframe.index[m[i] + 1], bts_dataframe.index[m[i + 1] + 1], m[i + 1] - m[i]])
 
     return np.array(discharges_list)
 
@@ -51,13 +50,45 @@ The selection criteria are:
     3. Store all the discharges starting and ending index in a list!!!!
 
 """
-all_BTS_discharges = []
-bts_list_final_Upd = []
+all_BTS_discharges_dict = {}
 
 # Process each bts_id!
 for bts_id in unique_bts_ids:
     bts_considered = pd.DataFrame(list(collection.find({"bts_id": bts_id})))
+    bts_considered.set_index('timestamp', inplace=True)
 
     BTS_Discharges = bts_discharges(bts_considered)
     BTS_Discharges_Sorted = BTS_Discharges[BTS_Discharges[:, 2].astype(int).argsort()[::-1]]
 
+    updated_discharges = []
+    for key in BTS_Discharges_Sorted:
+        # Shift times
+        step = pd.Timedelta(minutes=5)
+        start_shifted = key[0] + step
+        end_shifted = key[1] - step
+
+        if min(bts_considered.loc[start_shifted:end_shifted]['Battery Voltage(V)']) < 46.2:
+
+            # Create list of timestamps from start to end with 5-minute intervals
+            time_range = pd.date_range(start=start_shifted, end=end_shifted, freq=step)
+
+            for t in time_range:
+                # If the battery load observed while discharging is less than 20% of what is seen
+                # a step before, that is the battery discharge end!
+                # This is to leave out entries after the battery is empty!!!
+                if (abs(bts_considered.loc[t]['Battery Current(A)']) <
+                        abs(0.2 * bts_considered.loc[t-step]['Battery Current(A)'])):
+                    end_shifted = t
+                    break
+            updated_discharges.append([start_shifted, end_shifted])
+
+    if updated_discharges:
+        # Checks if the list is not empty!
+        all_BTS_discharges_dict[bts_id] = updated_discharges
+
+# Store the bts discharges!
+os.makedirs(output_dir, exist_ok=True)
+output_path = os.path.join(output_dir, 'all_BTS_discharges_dict.pkl')
+
+with open(output_path, 'wb') as handle:
+    pickle.dump(all_BTS_discharges_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
