7c4568bfc03836251e5fc89693906f994a44cc68
Yonas Y <yoni99y@gmail.com>
2025-08-07 17:09:46 +0300

    Update in parameter naming

--- app/config.py
+++ app/config.py
@@ -14,7 +14,7 @@ client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=3000)
 # Paths!
 operational_data_dir = Path("../../../Datasets/BTS/csv_files/operational_data/used_btss")
 fault_alarm_data_dir = Path("../../../Datasets/BTS/csv_files/fault/")
-output_dir = Path("output/")
+OUTPUT_DIR = Path("output/")
 
 # Configurable parameters
 sampling_time = 5 / 60  # Sampling time of the time series data!
