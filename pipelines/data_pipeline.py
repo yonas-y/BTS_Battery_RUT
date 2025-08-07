2468729509c03278a5fb588e89256082e8f6572d
Yonas Y <yoni99y@gmail.com>
2025-08-07 17:13:58 +0300

    Added the discharge extraction step in the pipeline!

--- pipelines/data_pipeline.py
+++ pipelines/data_pipeline.py
@@ -1,18 +1,27 @@
 from zenml import pipeline
-from typing import Dict
 from steps.data_import_step import data_import_step
+from steps.discharges_extraction_step import discharges_extraction_step
+from app.config import OUTPUT_DIR
 from app.config import MONGO_URI, DATABASE_NAME, COLLECTION_NAME, UNIQUE_ID
+from typing import Tuple, Dict
 
 @pipeline(name="data_pipeline")
-def data_pipeline() -> Dict:
+def data_pipeline() -> Tuple[Dict, Dict]:
     """
     Pipeline to import data from MongoDB into the ZenML framework.
     """
-    # Step to import data
+    # Step to import data!
     bts_data = data_import_step(
         uri=MONGO_URI,
         db_name=DATABASE_NAME,
         collection_name=COLLECTION_NAME,
         feature=UNIQUE_ID
     )
-    return bts_data
+
+    # Step to extract discharges from the given bts data!
+    bts_discharges = discharges_extraction_step(
+        bts_data=bts_data,
+        output_dir=OUTPUT_DIR
+    )
+
+    return bts_data, bts_discharges
