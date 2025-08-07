cdbffe1243d93f85803c8ac71091df9ce96263e6
Yonas Y <yoni99y@gmail.com>
2025-08-07 16:53:28 +0300

    Added additional parameters used in extraction script

--- app/config.py
+++ app/config.py
@@ -2,7 +2,8 @@ from pathlib import Path
 from pymongo import MongoClient
 
 # MongoDB connection!
-MONGO_URI = "mongodb://192.168.80.1:27017"
+# MONGO_URI = "mongodb://192.168.80.1:27017"
+MONGO_URI = "mongodb://host.docker.internal:27017"
 DATABASE_NAME = "BTS_Dataset"
 COLLECTION_NAME = "bts_oper_data_soc"
 UNIQUE_ID = "bts_id"
@@ -15,8 +16,12 @@ operational_data_dir = Path("../../../Datasets/BTS/csv_files/operational_data/us
 fault_alarm_data_dir = Path("../../../Datasets/BTS/csv_files/fault/")
 output_dir = Path("output/")
 
-# Important parameters!
+# Configurable parameters
 sampling_time = 5 / 60  # Sampling time of the time series data!
+
+VOLTAGE_THRESHOLD = 46.2    # Considered fault below this voltage!
+TIME_STEP_MINUTES = 5       # Time step for processing discharges in minutes!
+
 
 # Selected BTSs to work on and their full sample positions.
 full_stamp_sample = {
